1d259521d416f50ae5bbab19b375f9eae11f4ed9
Caupolican <caupolicanquerales@gmail.com>
2022-02-03 15:17:19 -0300

    Proyecto inicial de log for kibana

---
+++ src/main/java/com/capo/logForKibana/LogForKibanaApplication.java
@@ -0,0 +1,13 @@
+package com.capo.logForKibana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogForKibanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogForKibanaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capo/logForKibana/LogForKibanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capo.logForKibana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogForKibanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
